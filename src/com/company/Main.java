c94fb2fc8b5c29e3f1b0f4ae6c096517f537ff0c
SofronieKevinDavid <davidsof01@gmail.com>
2019-04-28 14:23:55 +0300

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,10 @@ import static com.company.FromJavaToJavaPaths.fromPathToJavaPaths;
 public class Main {
 
     public static void main(String[] args) {
-        String one=args[0];
-        ArrayList<String> arrayList=fromPathToJavaPaths(one);
-        for(int i=0;i<arrayList.size();i++){
-            fromJavaPathToOutput(arrayList.get(i));
+        String argument=args[0];
+        ArrayList<String> javaFiles=fromPathToJavaPaths(argument);
+        for(int i=0;i<javaFiles.size();i++){
+            fromJavaPathToOutput(javaFiles.get(i));
         }
     }
 
